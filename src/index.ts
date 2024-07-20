d4e137c579e268b41c36fcf9d57a2d2b76df8881
gusdev06 <coven688@gmail.com>
2024-07-20 12:12:28 -0300

    config cors

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,13 @@ app.use(
     corsConfig
 );
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 
 app.use(express.json());
 
--- src/infra/middlewares/allowCors.ts
+++ src/infra/middlewares/allowCors.ts
@@ -4,6 +4,8 @@ import cors from 'cors';
 const corsOptions: cors.CorsOptions = {
     origin: '*', // Permitir todas as origens
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'], // Permitir todos os métodos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Permitir esses headers
+    credentials: true,
 };
 
 export default cors(corsOptions);
