ed2e7813b6466276d195823468a4ba9dac675530
gusdev06 <coven688@gmail.com>
2024-08-25 14:32:51 -0300

    refatorando rotas

--- src/infra/database/presenters/report-presenter.ts
+++ src/infra/database/presenters/report-presenter.ts
@@ -1,18 +1,17 @@
 import { Reports } from "@/domain/attendances/entities/value-objects/reports";
 
 export class ReportPresenter {
-    static toHTTP(report: Reports) {
-        return {
-            employeeName: report.employeeName,
-            interval: report.interval,
-            totalDelay: report.totalDelay,
-            totalOvertime: report.totalOvertime,
-            totalWorkedHours: report.totalWorkedHours,
-            absences: report.absences,
-            paidAbsences: report.paidAbsences,
-            workDays: report.workDays,
-            daysAbsences: report.daysAbsences
-        };
-    }
+	static toHTTP(report: Reports) {
+		return {
+			employeeName: report.employeeName,
+			interval: report.interval,
+			totalDelay: report.totalDelay,
+			totalOvertime: report.totalOvertime,
+			totalWorkedHours: report.totalWorkedHours,
+			absences: report.absences,
+			paidAbsences: report.paidAbsences,
+			workDays: report.workDays,
+			daysAbsences: report.daysAbsences,
+		};
+	}
 }
-
--- src/infra/http/controllers/attendance/generate-report-controller.ts
+++ src/infra/http/controllers/attendance/generate-report-controller.ts
@@ -1,7 +1,7 @@
 import { HttpStatusCode } from "@/core/constants/HttpStatusCode";
 import { IController } from "@/core/protocols/IController";
 import { EditFirstTimeUseCase } from "@/domain/attendances/use-cases/edit-firts-time";
-import {  GenerateReportUseCase } from "@/domain/attendances/use-cases/generate-report";
+import { GenerateReportUseCase } from "@/domain/attendances/use-cases/generate-report";
 
 import { AttendancePresenter } from "@/infra/database/presenters/attendance-presenter";
 import { ReportPresenter } from "@/infra/database/presenters/report-presenter";
@@ -9,36 +9,29 @@ import { ReportPresenter } from "@/infra/database/presenters/report-presenter";
 import { NextFunction, Request, Response } from "express";
 
 class GenerateReportController implements IController {
-    constructor(private readonly useCase: GenerateReportUseCase) {}
+	constructor(private readonly useCase: GenerateReportUseCase) {}
 
-    async handle(
-        request: Request,
-        response: Response,
-        next: NextFunction,
-    ): Promise<void | Response<any, Record<string, any>>> {
-        try {
-      
-            const { rfid, startDate, endDate } = request.params;
+	async handle(
+		request: Request,
+		response: Response,
+		next: NextFunction,
+	): Promise<void | Response<any, Record<string, any>>> {
+		try {
+			const { rfid, startDate, endDate } = request.params;
 
-            const result = await this.useCase.execute({
-                rfid,
-                startDate: new Date(startDate),
-                endDate: new Date(endDate),
-            });
+			const result = await this.useCase.execute({
+				rfid,
+				startDate: new Date(startDate),
+				endDate: new Date(endDate),
+			});
 
-            if (result.isLeft())
-                return response
-                    .status(HttpStatusCode.BAD_REQUEST)
-                    .json(result.value);
+			if (result.isLeft()) return response.status(HttpStatusCode.BAD_REQUEST).json(result.value);
 
-            return response
-                .status(HttpStatusCode.OK)
-                .json(ReportPresenter.toHTTP(result.value?.report));
-        } catch (error) {
-            next(error);
-        }
-    }
+			return response.status(HttpStatusCode.OK).json(ReportPresenter.toHTTP(result.value?.reports));
+		} catch (error) {
+			next(error);
+		}
+	}
 }
 
 export { GenerateReportController };
-
